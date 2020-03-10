2edf233b7ead4e5c5f64763790a58dc3ab200fcd
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-10 04:45:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    katz_deli.flatmap()
+    katz_deli.flat_map
     puts "The line is currently: "
 end    
   
